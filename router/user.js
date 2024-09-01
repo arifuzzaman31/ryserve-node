1d1112cdf2deb45bea18eec9602745ebe484a8f8
arifuzzaman35 <arif.zaman@webable.digital>
2024-09-01 14:39:40 +0600

    test uri added

--- router/user.js
+++ router/user.js
@@ -6,5 +6,9 @@ const cuisine_controller = require("../controller/backend/cuisine");
 routes.get("/sub-asset-component", subassetcomp_controller.subassetcomp_list);
 routes.get("/sub-asset-component/:id", subassetcomp_controller.get_subassetcomp);
 routes.get("/cuisine", cuisine_controller.cuisine_list);
+routes.get("/info",async(req,res) => {
+    res.status(200).send(window.location.host)
+    return ;
+});
 
 module.exports = routes;
