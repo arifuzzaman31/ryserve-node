499cae11ee460dbf7aa475d1f0c270cb801d4117
arifuzzaman35 <arif.zaman@webable.digital>
2024-09-02 15:36:21 +0600

    cors added

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const userRoute = require('./router/user');
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PATCH, PUT, DELETE");
+    next();
+})
 app.use(express.json());
-
 app.use('/api/backend', adminRoute);
 app.use('/api', [authRoute, userRoute]);
 
