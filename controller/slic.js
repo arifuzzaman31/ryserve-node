94ea63d28068a0640d7c0479198436352a3edb9e
arifuzzaman35 <arif.zaman@webable.digital>
2024-11-12 15:38:23 +0600

    slic add to ryserve

---
+++ controller/slic.js
@@ -0,0 +1,53 @@
+const asyncHandler = require("express-async-handler");
+const https = require('https');
+const axios = require('axios');
+
+exports.slic_token = asyncHandler(async (req, res) => {
+  try {
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: false,
+    });
+
+    const resp = await axios.post(
+      "http://120.50.12.81:8501/token",
+      {
+        "ApiKey": "$2b$12$rg.troP82UZf9a4xH3PBBOHWvKKzAOhrCyy4i29df4HDquDGe7nzK",
+        "UserName": "WebAble"
+      },
+      {
+        httpsAgent,
+      }
+    );
+    return res.status(200).send(resp.data);
+  } catch (error) {
+    console.error('Error making request:', error);
+    return res.status(500).send({ error: 'Failed to make request' });
+  }
+});
+
+exports.prem_cal = asyncHandler(async (req, res) => {
+    const param = await req.body;
+    const apiUrl = `http://120.50.12.81:8501/prem_calculator?product_cd=${param.productCD}&term=${param.terms}&sum_assured=${param.sumAssure}&pay_mode=${param.paymentModeCode}&age=${param.age}`;
+    try {
+        const httpsAgent = new https.Agent({
+          rejectUnauthorized: false,
+        });
+    
+        const resp = await axios.get(
+            apiUrl,
+          {
+            headers: {
+              Authorization: `Bearer ${param.token}`,
+              "Content-Type": "application/json",
+            }
+          },
+          {
+            httpsAgent,
+          }
+        );
+        return res.status(200).send(resp.data);
+      } catch (error) {
+        console.error('Error making request:', error);
+        return res.status(500).send({ error: 'Failed to make request' });
+      }
+});
--- router/user.js
+++ router/user.js
@@ -2,8 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const subassetcomp_controller = require("../controller/frontend/subassetcomp");
 const cuisine_controller = require("../controller/backend/cuisine");
-const https = require('https');
-const axios = require('axios');
+const slic_controller = require("../controller/slic");
 
 routes.get("/sub-asset-component", subassetcomp_controller.subassetcomp_list);
 routes.get("/sub-asset-component/:id", subassetcomp_controller.get_subassetcomp);
@@ -11,27 +10,8 @@ routes.get("/cuisine", cuisine_controller.cuisine_list);
 routes.get("/check-cd", async(req,res) => {
       return res.status(200).send("Hello Bello");
 });
-routes.post("/slic-token", async(req,res) => {
-      try {
-            const httpsAgent = new https.Agent({
-              rejectUnauthorized: false,
-            });
-        
-            const resp = await axios.post(
-              "http://120.50.12.81:8501/token",
-              {
-                "ApiKey": "$2b$12$rg.troP82UZf9a4xH3PBBOHWvKKzAOhrCyy4i29df4HDquDGe7nzK",
-                "UserName": "WebAble"
-              },
-              {
-                httpsAgent,
-              }
-            );
-            return res.status(200).send(resp.data);
-          } catch (error) {
-            console.error('Error making request:', error);
-            return res.status(500).send({ error: 'Failed to make request' });
-          }
-});
+
+routes.post("/slic-token",slic_controller.slic_token);
+routes.post("/prem-cal",slic_controller.prem_cal);
 
 module.exports = routes;
